4f37cc8d3da78079c081de1f14ee606ffdfdd5c1
amanosan121 <amanosan121@gamil.com>
2020-10-06 01:15:09 +0530

    initial commit

---
+++ news_reader.py
@@ -0,0 +1,32 @@
+import requests
+from win32com.client import Dispatch
+import time
+
+
+def speak(s):
+    speaker = Dispatch("SAPI.SpVoice")
+    speaker.Speak(s)
+
+
+def get_top_headlines(news_api_url):
+    response = requests.get(news_api_url)
+
+    news = response.json()
+    articles = news['articles']
+    c = []
+    for i in articles:
+        c.append(i['title'] + i['description'])
+
+    return c
+
+
+if __name__ == '__main__':
+    url = (f'http://newsapi.org/v2/top-headlines?'
+           'country=in&'
+           'apiKey=f9888439e1c043b491a9c318d6e653af')
+
+    content = get_top_headlines(url)
+
+    for x in content:
+        speak(x)
+        time.sleep(3)
